1021c83754985f74743be1d19fce8386825d16da
Thariq <thariq879@gmail.com>
2023-02-03 14:32:35 +0700

    used multiple class based view

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ReviewView.as_view()),
     path('thank-you/', views.thankyou),
+    path('review-list/', views.ReviewListView.as_view()),
+    path('review-list/<int:pk>', views.SingleReviewView.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,30 +1,71 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import FormView
+
 from .forms import MyForm
 from .models import Review
-from django.views import View
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = MyForm()
+# ReviewView using FormView
+class ReviewView(FormView):
+    form_class = MyForm
+    template_name = "reviews/reviews.html"
+    success_url = "/thank-you"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+# Using custom view
+#
+# class ReviewView(View):
+#     def get(self, request):
+#         form = MyForm()
         
-        return render(request,'reviews/reviews.html', {
-        'form': form
-        })
+#         return render(request,'reviews/reviews.html', {
+#         'form': form
+#         })
 
-    def post(self, request):
-        form = MyForm(request.POST)
+#     def post(self, request):
+#         form = MyForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank-you')
-
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('/thank-you')
 
 def thankyou(request):
-    form = Review.objects.all() 
     latest_username = Review.objects.latest('user_name') # get latest index selected column
     return render(request, 'reviews/thank_you.html',{
         'latest_username' : latest_username.user_name  # select lastest 'user_name'
     })
+
+class ReviewListView(ListView):
+    model = Review
+    template_name = "reviews/list_review.html"
+    context_object_name = "reviews"  # to retrieve data to view as name reviews
+    
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=4)
+        return data
+
+
+# class SingleReviewView(TemplateView):
+#     template_name = "reviews/single_review.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+
+#         review_id = kwargs['id']
+#         selected_review = Review.objects.get(pk=review_id)
+
+#         context["reviews"] = selected_review
+#         return context
+    
+class SingleReviewView(DetailView):
+    model = Review
+    template_name = "reviews/single_review.html"
+    context_object_name = "reviews"
