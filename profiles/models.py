8116715c24153a83f12991a13a4080519146e573
Thariq <thariq879@gmail.com>
2023-02-07 10:13:51 +0700

    using ImageFields

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
